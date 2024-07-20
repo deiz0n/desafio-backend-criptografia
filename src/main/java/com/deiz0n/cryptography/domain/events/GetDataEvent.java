8ab332831cb4519bf738c4832f5c606fe7d0bdd3
deiz0n <eduardomacedosilva@outlook.com>
2024-07-20 14:52:56 -0300

    refactor: Ajustes nos GetDataEvent e GetListOfDataEvent

--- src/main/java/com/deiz0n/cryptography/domain/events/GetDataEvent.java
+++ src/main/java/com/deiz0n/cryptography/domain/events/GetDataEvent.java
@@ -1,11 +1,17 @@
 package com.deiz0n.cryptography.domain.events;
 
+import com.deiz0n.cryptography.domain.dtos.UserDTO;
+import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 
+@Getter
 public class GetDataEvent extends ApplicationEvent {
 
-    public GetDataEvent(Object source) {
+    private UserDTO user;
+
+    public GetDataEvent(Object source, UserDTO user) {
         super(source);
+        this.user = user;
     }
 
 }
--- src/main/java/com/deiz0n/cryptography/domain/events/GetListOfDataEvent.java
+++ src/main/java/com/deiz0n/cryptography/domain/events/GetListOfDataEvent.java
@@ -1,11 +1,19 @@
 package com.deiz0n.cryptography.domain.events;
 
+import com.deiz0n.cryptography.domain.dtos.UserDTO;
+import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 
+import java.util.List;
+
+@Getter
 public class GetListOfDataEvent extends ApplicationEvent {
 
-    public GetListOfDataEvent(Object source) {
+    private List<UserDTO> users;
+
+    public GetListOfDataEvent(Object source, List<UserDTO> users) {
         super(source);
+        this.users = users;
     }
 
 }

e7915a21ae6c07dac6c6693a2b0c6eb204f518f5
deiz0n <eduardomacedosilva@outlook.com>
2025-01-17 16:26:35 -0300

    feat: Adição do builder no User

--- src/main/java/com/deiz0n/cryptography/domain/entities/User.java
+++ src/main/java/com/deiz0n/cryptography/domain/entities/User.java
@@ -5,11 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
